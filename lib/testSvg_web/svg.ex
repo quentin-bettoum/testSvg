bf28c83a2199c64cdd616639c72eaac4d71c0b1d
Quentin BETTOUM <quentin.bettoum@mailo.com>
2022-08-02 11:18:11 +0200

    Add svg files as external_resources to force recompile on change

--- lib/testSvg_web/svg.ex
+++ lib/testSvg_web/svg.ex
@@ -1,7 +1,18 @@
 defmodule TestSvgWeb.Svg do
+  @svg_path "assets/svg"
+  @svg_path_wildcard Path.wildcard(@svg_path <> "/**/*.svg")
+  @paths_hash :erlang.md5(@svg_path_wildcard)
+
+  for path <- @svg_path_wildcard do
+    @external_resource path
+  end
+
+  def __mix_recompile__?() do
+    :erlang.md5(@svg_path_wildcard) != @paths_hash
+  end
 
   # Build the library at compile time
-  @library Adept.Svg.compile( "assets/svg" )
+  @library Adept.Svg.compile( @svg_path )
 
   # Accesses the library at run time
   defp library(), do: @library
